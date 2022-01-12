e242a706ee17c8475e6242c7e12d33faa694d103
Alex <alexmcgil@vivaldi.net>
2022-01-12 22:10:01 +0300

    <Almost finished the layout>

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import "./App.css"
 import Header from "./Components/Header/Header";
 import BackGround from "./Components/Background/BackGround";
 import Footer from "./Components/Footer/Footer";
+import Game from "./Components/Game/Game";
 
 function App(){
 
 	return <div>
 		<Header />
 		<BackGround />
+		<Game />
 		<Footer />
 	</div>
 }
---
+++ src/Components/Game/Game.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Game.css"
+import Levels from "./Levels/Levels";
+import Picture from "./Picture/Picture";
+
+function Game() {
+    return (
+        <main>
+            <Levels />
+            <Picture />
+        </main>)
+}
+
+export default Game
